68bad731cdeb42dcd607167ca7a4f4f3cc74399c
alphonse SIBOMANA <alphonsesibomana@alphonses-MacBook-Pro.local>
2023-02-13 08:35:24 +0200

    added new effect on scroll, redesigned services

--- js/nav.js
+++ js/nav.js
@@ -1,43 +1,43 @@
-//sliders
-var slideIndex = 0;
-var slider = document.querySelector(".slider");
-var cards = document.querySelectorAll(".card");
+// //sliders
+// var slideIndex = 0;
+// var slider = document.querySelector(".slider");
+// var cards = document.querySelectorAll(".card");
 
-function showSlides() {
-  for (let i = 0; i < cards.length; i++) {
-    if (i >= slideIndex && i < slideIndex + 4) {
-      cards[i].style.display = "inline-block";
-    } else {
-      cards[i].style.display = "none";
-    }
-  }
-  if (slideIndex === 0) {
-    document.querySelector(".prev-btn").style.display = "none";
-  } else {
-    document.querySelector(".prev-btn").style.display = "block";
-  }
-  if (slideIndex === cards.length - 4) {
-    document.querySelector(".next-btn").style.display = "none";
-  } else {
-    document.querySelector(".next-btn").style.display = "block";
-  }
-}
+// function showSlides() {
+//   for (let i = 0; i < cards.length; i++) {
+//     if (i >= slideIndex && i < slideIndex + 4) {
+//       cards[i].style.display = "inline-block";
+//     } else {
+//       cards[i].style.display = "none";
+//     }
+//   }
+//   if (slideIndex === 0) {
+//     document.querySelector(".prev-btn").style.display = "none";
+//   } else {
+//     document.querySelector(".prev-btn").style.display = "block";
+//   }
+//   if (slideIndex === cards.length - 4) {
+//     document.querySelector(".next-btn").style.display = "none";
+//   } else {
+//     document.querySelector(".next-btn").style.display = "block";
+//   }
+// }
 
-document.querySelector(".prev-btn").addEventListener("click", function () {
-  slideIndex--;
-  showSlides();
-});
+// document.querySelector(".prev-btn").addEventListener("click", function () {
+//   slideIndex--;
+//   showSlides();
+// });
 
-document.querySelector(".next-btn").addEventListener("click", function () {
-  slideIndex++;
-  showSlides();
-});
+// document.querySelector(".next-btn").addEventListener("click", function () {
+//   slideIndex++;
+//   showSlides();
+// });
 
-showSlides();
+// showSlides();
 
-//date on the footer
-const date = document.getElementById('date')
-date.innerHTML = new Date().getFullYear()
+// //date on the footer
+// const date = document.getElementById('date')
+// date.innerHTML = new Date().getFullYear()
 
 //nav 
 const mobileMenu = document.querySelector('.nav-menu')
@@ -45,14 +45,31 @@ const navToggle = document.querySelector('.nav-toggle')
 const closeBtn = document.querySelector('.nav-close-btn')
 const headerBg = document.querySelector('.header')
 const navLinks = document.querySelectorAll('.link')
+const navLink = document.querySelectorAll('.nav-link')
+const logoBlue = document.querySelector('.blue-logo')
+const logoWhite = document.querySelector('.white-logo')
 
 window.addEventListener('scroll', () => {
   let scrolledPixels = window.scrollY
 
   if (scrolledPixels >= 50) {
-    headerBg.style.background = '#00000077'  
-  }else {
+    headerBg.style.background = 'white'
+    headerBg.style.boxShadow = "10px 20px 50px rgba(0, 0, 0, 0.070)";
+    logoBlue.style.display = 'block'
+    logoWhite.style.display = 'none'
+    navToggle.style.color = '#4565af'
+    navLink.forEach((link) => {
+      link.style.color = '#4565af'
+    })
+  } else {
     headerBg.style.background = 'transparent'
+    headerBg.style.boxShadow = 'none'
+    logoBlue.style.display = 'none'
+    logoWhite.style.display = 'block'
+    navToggle.style.color = 'white'
+    navLink.forEach((link) => {
+      link.style.color = 'white'
+    })
   }
 })
 
@@ -62,7 +79,7 @@ window.addEventListener('scroll', () => {
 
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
-    if (window.innerWidth < 1200 ) {
+    if (window.innerWidth < 1200) {
       mobileMenu.style.display = 'none'
     }
   })
@@ -74,12 +91,24 @@ navToggle.addEventListener('click', () => {
   mobileMenu.style.display = 'block'
   closeBtn.style.display = 'block'
   navToggle.style.display = 'none'
+  headerBg.style.background = 'white'
+  logoBlue.style.display = 'block'
+  logoWhite.style.display = 'none'
+  navLink.forEach((link) => {
+    link.style.color = '#4565af'
+  })
 })
 // closing the fixed nav
 closeBtn.addEventListener('click', () => {
   mobileMenu.style.display = 'none'
   closeBtn.style.display = 'none'
   navToggle.style.display = 'block'
+  logoBlue.style.display = 'none'
+  logoWhite.style.display = 'block'
   headerBg.style.background = 'transparent'
+
+  navLink.forEach((link) => {
+    link.style.color = 'white'
+  })
 })
 
